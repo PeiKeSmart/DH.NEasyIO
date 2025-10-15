29e4f3fa241484e53e25e2301d8dee9a1b88f7ce
猿人易 <qcjxberin@outlook.com>
2025-10-15 15:31:48 +0800

    更新框架支持并新增文件控制器
    移除对 .NET 6 和 .NET 7 的支持，仅保留对 .NET 8、.NET 9 和 .NET 10 的支持。
    新增对 Pek.NCubeUI 包的依赖，版本为 4.14.2025.1004-beta0530。
    
    新增 IOController 控制器类，用于处理文件存储逻辑：
    - 支持通过注入 FileStorageOptions 配置文件路径。
    - 实现了路径生成方法 GetPath 和视图返回方法 Index。

---
+++ Pek.EasyIO/Controllers/IOController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using NewLife;
+
+using Pek.EasyIO.Options;
+using Pek.NCube;
+
+namespace Pek.EasyIO.Controllers;
+
+/// <summary>文件控制器</summary>
+public class IOController : PekBaseControllerX
+{
+    private readonly FileStorageOptions _storageOptions;
+
+    /// <summary>实例化</summary>
+    /// <param name="storageOptions"></param>
+    public IOController(FileStorageOptions storageOptions) => _storageOptions = storageOptions;
+
+    private String GetPath(String id)
+    {
+        if (id.IsNullOrEmpty()) throw new ArgumentNullException(nameof(id));
+
+        var set = _storageOptions;
+        if (set.Path.IsNullOrEmpty()) throw new Exception("未配置存储信息");
+
+        return set.Path.CombinePath(id).GetFullPath();
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
