2c8cdd12372d282d08c030b1bf62dbe6f612b7e9
猿人易 <qcjxberin@outlook.com>
2026-01-16 16:31:02 +0800

    build: 更新多个项目的依赖包版本
    - 升级 XUnitTest 项目的 Microsoft.Extensions.DependencyInjection 到 10.0.2
    - 更新 DH.NEasyIO 和 Pek.EasyIO 项目的 DH.NCore、DH.NStardust.Extensions 和 Microsoft.SourceLink.GitHub 版本
    - 升级 Pek.EasyIO 项目的 Pek.NCubeUI 到 4.16.2026.113-beta1126
    - 新增 Pek.EasyIO 项目的 CleanupJobService 定时任务服务

---
+++ Pek.EasyIO/Jobs/CleanupJobService.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+
+using NewLife.Cube.Jobs;
+using NewLife.Log;
+
+namespace Pek.EasyIO.Jobs;
+
+/// <summary>
+/// 刷新大屏页面作业参数
+/// </summary>
+public class CleanupJobArgument
+{
+
+}
+
+/// <summary>刷新大屏页面服务</summary>
+[DisplayName("刷新大屏页面")]
+[Description("刷新大屏页面")]
+[CronJob("CleanupJob", "0 */10 * * * ? *", Enable = true)]
+public class CleanupJobService : CubeJobBase<CleanupJobArgument>
+{
+    private readonly ITracer _tracer;
+
+    /// <summary>实例化检查固件升级服务</summary>
+    /// <param name="tracer"></param>
+    public CleanupJobService(ITracer tracer)
+    {
+        _tracer = tracer;
+    }
+
+    /// <summary>执行作业</summary>
+    /// <param name="argument"></param>
+    /// <returns></returns>
+    protected override async Task<String> OnExecute(CleanupJobArgument argument)
+    {
+        using var span = _tracer?.NewSpan("CleanupJob", argument);
+
+
+        
+
+        return "OK";
+    }
+}
